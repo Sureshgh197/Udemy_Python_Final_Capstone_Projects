7a9efa1821a83215c53de7296601f0262cceac8c
Sureshgh197 <srshbtt@gmail.com>
2022-02-12 22:39:42 +0530

    from lap onverter

--- 11_converter.py
+++ 11_converter.py
@@ -1,13 +1,14 @@
-import sys
 
+
+ 
 class Converter:
 	
 	def __init__(self,metric1,value):
 		self.metric1=metric1
 		self.value=value
-		
-		
-		
+		 
+		 
+		 
 	def temparatureConverter(self):
 		# centigrade to fahrenhrit
 		#fshrenheut to  centigrsde
@@ -16,40 +17,52 @@ class Converter:
 			print(f'Fahrenheit of {self.value} °C is {(self.value*(9/5)+32)}')
 		else:
 			print(f'Fahrenheit of {self.value} °C is {(self.value-32)*(5/9)}')
-			
+			 
 	def convert_Length(self, m2):
-	   Ll=['mm','cm','m','km']
+	   L=['mm','cm','m','km']
 	   m1v=self.value
-	   m1=Ll[self.metric1-1]
-	   m2t=Ll[m2-1]
-	   SI ={'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000.}
-	   print(m1v*(SI[m1]/SI[m2t]))
-       
+	   m1=L[self.metric1-1]
+	   m2t=L[m2-1]
+	   Lc ={'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000.}
+	   print(m1v*(Lc[m1]/Lc[m2t]))
+	    
+	def convert_Volume(self,m2):
+	      V = ['lt','mlt','cm','cc']
+	      m1v=self.value
+	      m1=V[self.metric1-1]
+	      m2t=V[m2-1]
+	      Vc = {'cc':1, 'cm':1000000,'mlt':1,'lt':1000}
+	      print(m1v*(Vc[m1]/Vc[m2t]))
+		   
+        
 while True:
-    	
+    	 
 		print('Choose the following Converters select corresponding numbers\n')
-		cat = int(input('1. Temparature\n2. Length\n3. Mass\n4. Currency\n'))
+		cat = int(input('1. Temparature\n2. Length\n3. Volume\n4. Currency\n'))
 		if cat == 1:
 			print('Choose the following  available converters\n')
 			metric1 = int(input('1. Centigrade to Fahrenheit\n2. Fahrenheit to Centigrade\n'))
 			value = float(input('Enter the value:\n'))
 			conv = Converter(metric1,value)
 			conv.temparatureConverter()
-			
-			
+			 
+			 
 		elif cat == 2:
 			print('Choose the following  available converters\n')
-			
+			 
 			print('1. Millimeter\n2. Centimeter\n3. Meter\n4. Kilometer')
-			
-			m1=int(input('Please privide the From converter: Eg: 3 for meter\n'))
+			 
+			m1=int(input('Please choose the From converter: Eg: 3 for meter\n'))
 			m1v=float(input('Please provide from Value'))
-			
-			m2=int(input('Please privide the To converter:\n'))
+			 
+			m2=int(input('Please choose the To converter:\n'))
 			conv = Converter(m1,m1v)
 			conv.convert_Length(m2)
-	
-			
-			
-		
-		
+		elif cat == 3:
+			print('1. Litres\n2. Milli litres\n3. Cubic metres\n4. Cubic Centimetres')
+			m1=int(input('Please choose the From converter: Eg: 3 for Cubic metre\n'))
+			m1v=float(input('Please provide from Value'))
+			 
+			m2=int(input('Please choose the To converter:\n'))
+			conv = Converter(m1,m1v)
+			conv.convert_Volume(m2)
